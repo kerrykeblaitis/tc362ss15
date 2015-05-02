103f95b0af3c91ca99bcae00b61b77923f5ee8ea
Kerry Keblaitis <keblait1@msu.edu>
2015-05-02 01:35:54 -0400

    webpage

---
+++ final_project/js/images.js
@@ -0,0 +1,38 @@
+function OnPressImage1()
+{
+	// Find the large image element
+	var imageElement = document.getElementById("large-image");
+
+	// Sets its source to the new image.
+	imageElement.src = "img/feature-small01.jpg";
+
+}
+
+function OnPressImage2()
+{
+	// Find the large image element
+	var imageElement = document.getElementById("large-image");
+
+	// Sets its source to the new image.
+	imageElement.src = "img/feature-small02.jpg";
+
+}
+
+function OnPressImage3()
+{
+	// Find the large image element
+	var imageElement = document.getElementById("large-image");
+
+	// Sets its source to the new image.
+	imageElement.src = "img/feature-small03.jpg";
+
+}
+function OnPressImage4()
+{
+	// Find the large image element
+	var imageElement = document.getElementById("large-image");
+
+	// Sets its source to the new image.
+	imageElement.src = "img/feature-small04.jpg";
+
+}
